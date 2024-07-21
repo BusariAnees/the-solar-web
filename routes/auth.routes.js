ed33b7402a75dde3697086a7eacdf5bf326539ad
Anees <ayomideanees2003@icloud.com>
2024-07-21 09:05:27 +0100

    added database

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -7,6 +7,7 @@ const router  = express.Router();
 
 router.get('/signup', authController.getSignup);
 
+router.post('/signup', authController.signup);
 
 router.get('/login', authController.getLogin);
 
