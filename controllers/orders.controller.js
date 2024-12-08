8999eac553a30e279f5a881c0c694df7f74bbb8a
Anees <ayomideanees2003@icloud.com>
2024-12-08 12:53:07 +0100

    finished adding stripe payments

--- controllers/orders.controller.js
+++ controllers/orders.controller.js
@@ -1,3 +1,6 @@
+const stripe = require('stripe')('sk_test_51QTiBwRpCowEiAwJhqRAOjDqjQw31A2equnLlonEUqIMXwGH27N83MthC5BsU7rsbfqoe0elr0wH8p1bHgw4B1U200vFhJgS9m');
+
+
 const Order = require('../models/order.model');
 const Auth = require('../models/auth');
 
@@ -40,12 +43,42 @@ async function addOrder (req, res, next) {
 
    req.session.cart = null;
 
-    res.redirect('/orders');
 
 
+  const session = await stripe.checkout.sessions.create({
+    line_items: cart.item.map(function(item) {
+      return  {
+        // Provide the exact Price ID (for example, pr_1234) of the product you want to sell
+        price_data: {
+          currency: 'usd',    // created by us
+          product_data: {
+            name: item.product.title,
+          },
+          unity_amount: +item.product.price.toFixed(2) * 100 // its in pennies thats why it is multiplied by 100
+        },
+        quantity: item.quantity,
+      }
+    }) ,
+    mode: 'payment',
+    success_url: `http://localhost:3000/orders/success`,
+    cancel_url: `http://localhost:3000/orders/failure`,
+});
+
+  res.redirect(303, session.url);
+
+}
+
+function getSuccess(req, res) {
+  res.render('customer/orders/success');
+}
+
+function getFailure(req, res) {
+  res.render('customer/orders/failure');
 }
 
 module.exports = {
     addOrder: addOrder,
     getOrders: getOrders,
+    getSuccess: getSuccess,
+    getFailure: getFailure,
 }
